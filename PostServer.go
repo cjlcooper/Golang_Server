49430069c5723132c9f82cd882269ad203a3b1e1
cjlcooper <cjlcooper@163.com>
2018-01-05 09:40:01 +0800

    Update PostServer.go

--- PostServer.go
+++ PostServer.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"encoding/json"
+	"time"
 )
 
 //type struct
@@ -15,11 +16,17 @@ type ResponseDict struct {
 }
 
 func main() {
+	// print start string
+	fmt.Println(returnValueTest())
+	//
 	http.HandleFunc("/test",test)
 	http.HandleFunc("/getJson",getJson)
+	http.HandleFunc("/concurrent",concurrent)
+	//start Server
 	http.ListenAndServe(":8080",nil)
 }
 
+// test
 func test(w http.ResponseWriter,r *http.Request) {
 	defer r.Body.Close()
 	var dict map[string]interface{}
@@ -30,15 +37,33 @@ func test(w http.ResponseWriter,r *http.Request) {
 	fmt.Fprintln(w, dict)
 }
 
+// get json
 func getJson(w http.ResponseWriter,r *http.Request) {
 	defer r.Body.Close()
 	var dict map[string]interface{}
 	con, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(con,&dict)
+	//
 	var responseDict = ResponseDict{}
 	responseDict.Name = "Hello Client"
 	responseDict.ID = "001"
 	responseDict.Code = "0"
 	var data,_ = json.Marshal(responseDict)
 	fmt.Fprintln(w, string(data))
+}
+
+// concurrent
+func concurrent(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	go saveInDB()
+}
+
+func saveInDB() {
+	fmt.Println("Hello,goroutine")
+}
+
+// return value
+func returnValueTest() (string) {
+	fmt.Println(time.Now())
+	return "Start Server ..."
 }
